e1b6456b81ea5c69ac99ce6bdc32b5c90eae43d5
Creatimatix <creatimatix@gmail.com>
2022-03-30 22:25:47 +0530

    about use map removal

--- pages/AboutUs.php
+++ pages/AboutUs.php
@@ -54,31 +54,32 @@
         <div class="col-md-12">
             <div class="titlepage">
                 <h2>About Us</h2>
-                <p></p>
+                <div class="slogan"><h3 style="
+    font-size: 27px; color: rgb(255, 199, 44);; font-family: 'bodoniflfmedium_italic'; font-style: italic;">Weaving dreams into precious stones to create jewellery that dazzle every gaze.</h3></div>
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
             <div class="contact">
-            <p> Established in 2008 Vaishnav Creation is India's leading findings jewellery company. We are independent business specialized in miracle plates.<br>
+            <p><h4> Established in 2008 Vaishnav Creation is India's leading findings jewellery company. We are independent business specialized in miracle plates.</br></br>
 Our journey is lovingly handcrafted in our very own purpose build workshop, which is based interest of Vasai taluka, Maharashtra. Each exquisitely made in precious metal. Our highly skilled team of designers, craftsmen and specialists are dedicated for creating and producing new stunning designs, which are worn and cherished by jewellery lovers from all around the world.
-We specialise in miracle plate ranging from 1.5mm to 10mm in various cuts and shapes. i.e. round, princess, marquise, pears, oval, heart and flower.<br>
-Here, at Vaishnav Creation you will be spoilt for choices with our range of miracle plates. If you are looking for something more personalised than our experts would be delighted to help you create a beautiful and timeless masterpiece of jewellery.<br>
-Whatever the case maybe, if you have something very specific in mind or a particular shape, design or cut, our team of craftsmen can tailor your design to meet that exact requirement, creating piece of timeless jewellery which will be with you and your family for many years to come.<br>
+We specialise in miracle plate ranging from 1.5mm to 10mm in various cuts and shapes. i.e. round, princess, marquise, pears, oval, heart and flower.</br></br>
+Here, at Vaishnav Creation you will be spoilt for choices with our range of miracle plates. If you are looking for something more personalised than our experts would be delighted to help you create a beautiful and timeless masterpiece of jewellery.</br></br>
+Whatever the case maybe, if you have something very specific in mind or a particular shape, design or cut, our team of craftsmen can tailor your design to meet that exact requirement, creating piece of timeless jewellery which will be with you and your family for many years to come.<br></h4>
 </p>
             </div>
         </div>
-        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+        <!-- <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
             <div class="rable-box">
                 <figure>
                 <img src="images/cac.png" alt="#" />
             </div>
-        </div>
+        </div> -->
     </div>
     </div>
 </div>
-<div class="responsive-map">
+<!-- <div class="responsive-map">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60189.36619664327!2d72.74298891835149!3d19.462662815969995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7aa49b828c6a5%3A0xbefe4c41a14ad251!2sVirar%20West%2C%20Virar%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1648368207971!5m2!1sen!2sin" width="100%" height="450" style="border:0;margin-bottom: -9px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
+</div> -->
 <!-- end contact -->
