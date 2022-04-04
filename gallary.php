87a38edaae50ae259436b527743e386cfa99350f
Creatimatix <creatimatix@gmail.com>
2022-04-05 00:58:48 +0530

    gallery fixed

--- gallary.php
+++ gallary.php
@@ -164,7 +164,17 @@
 
     
 
-    <script>
+     <!-- Javascript files-->
+     <!-- <script src="js/jquery.min.js"></script> -->
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
         $('.portfolio-menu ul li').click(function(){
         $('.portfolio-menu ul li').removeClass('active');
         $(this).addClass('active');
@@ -185,15 +195,5 @@
             });
         });
     </script>
-     <!-- Javascript files-->
-     <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <script src="js/plugin.js"></script>
-      <!-- sidebar -->
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>
-      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
    </body>
 </html>
