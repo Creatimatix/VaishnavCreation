f07ade5b55689c2db9a762d4e4f6ddef3d2f88d6
Creatimatix <creatimatix@gmail.com>
2022-04-08 08:07:44 +0530

    Gallery Images Updated with JS

--- gallary old.php
+++ gallary old.php
@@ -20,7 +20,7 @@
       <link rel="stylesheet" href="css/responsive.css">
       <!-- fevicon -->
       <link rel="icon" href="images/fevicon.png" type="image/gif" />
-      <script type="text/javascript" src="imageList.js"></script>
+      
       <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css" />
       <style>
@@ -98,25 +98,59 @@
                <li class="btn btn-outline-dark" data-filter=".others">Others</li>
             </ul>
          </div>
-         <div class="portfolio-item row" id="gallery_images">
-
-         <!-- for each image -->
-            <script>
-                var mydata = JSON.parse(data);
-                    // alert(mydata[0]);
-                    for(var i=0;i<mydata.length;i++){                        
-                    document.getElementById("gallery_images").innerHTML += 
-                        "<div class='item "+mydata[i].type+" col-lg-3 col-md-4 col-6 col-sm'>"
-                        +"<a href='"+mydata[i].path+"' class='fancylight popup-btn' data-fancybox-group='light'>"
-                        +"<img class='img-fluid' src='"+mydata[i].path+"' alt=''>"
-                        +"</a>"
-                        +"</div>";
-                    }
-            </script>
-           
-
-            <!-- Till here -->
-
+         <div class="portfolio-item row">
+           <div class="item rings col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://media.istockphoto.com/photos/fancy-designer-antique-golden-bracelets-for-woman-fashion-picture-id1277517088?b=1&k=20&m=1277517088&s=170667a&w=0&h=PXTQvh19pESR7mIekh3mJQHWcw2FDRrYcHdxsv9XY-Q=" class="fancylight popup-btn" data-fancybox-group="light"> 
+               <img class="img-fluid" src="https://media.istockphoto.com/photos/fancy-designer-antique-golden-bracelets-for-woman-fashion-picture-id1277517088?b=1&k=20&m=1277517088&s=170667a&w=0&h=PXTQvh19pESR7mIekh3mJQHWcw2FDRrYcHdxsv9XY-Q=" alt="">
+               </a>
+            </div>
+            <div class="item necklace col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1547738238-5ddb16bef15f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1547738238-5ddb16bef15f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" alt="">
+               </a>
+            </div>
+            
+            <div class="item rings col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1622398925373-3f91b1e275f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z29sZCUyMHJpbmd8ZW58MHx8MHx8&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1622398925373-3f91b1e275f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z29sZCUyMHJpbmd8ZW58MHx8MHx8&w=1000&q=80" alt="">
+               </a>
+            </div>
+            
+            <div class="item rings col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1638208170417-2464f9699df1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amV3ZWxyeSUyMGJveHxlbnwwfHwwfHw%3D&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1638208170417-2464f9699df1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amV3ZWxyeSUyMGJveHxlbnwwfHwwfHw%3D&w=1000&q=80" alt="">
+               </a>
+            </div>
+            <div class="item others col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1535632787350-4e68ef0ac584?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGpld2VsbGVyeXxlbnwwfHwwfHw%3D&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1535632787350-4e68ef0ac584?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGpld2VsbGVyeXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="">
+               </a>
+            </div>
+            <div class="item rings col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1528797664208-e5a8c0b98881?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amV3ZWxyeSUyMGRpc3BsYXl8ZW58MHx8MHx8&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1528797664208-e5a8c0b98881?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amV3ZWxyeSUyMGRpc3BsYXl8ZW58MHx8MHx8&w=1000&q=80" alt="">
+               </a>
+            </div>
+            <div class="item selfie col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1611591437281-460bfbe1220a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YnJhY2VsZXRzfGVufDB8fDB8fA%3D%3D&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1611591437281-460bfbe1220a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YnJhY2VsZXRzfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="">
+               </a>
+            </div>
+            <div class="item necklace col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGpld2VscnklMjBzaG9wfGVufDB8fDB8fA%3D%3D&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGpld2VscnklMjBzaG9wfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="">
+               </a>
+            </div>
+            <div class="item necklace col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://www.femalefirst.co.uk/image-library/land/1000/d/diamond-jewellery-stock-unsplash.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://www.femalefirst.co.uk/image-library/land/1000/d/diamond-jewellery-stock-unsplash.jpg" alt="">
+               </a>
+            </div>
+            <div class="item others col-lg-3 col-md-4 col-6 col-sm">
+               <a href="https://images.unsplash.com/photo-1612150354898-a69132eb7c67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGQlMjBqZXdlbHJ5fGVufDB8fDB8fA%3D%3D&w=1000&q=80" class="fancylight popup-btn" data-fancybox-group="light">
+               <img class="img-fluid" src="https://images.unsplash.com/photo-1612150354898-a69132eb7c67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGdvbGQlMjBqZXdlbHJ5fGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="">
+               </a>
+            </div>
          </div>
       </div>
       
---
+++ imageList.js
@@ -0,0 +1,12 @@
+data = `[
+{"name" : "ring", "path" : "images/galllery/1.png"}
+,{"name" : "ring", "path" : "images/galllery/2.png"}
+,{"name" : "ring", "path" : "images/galllery/3.png"}
+,{"name" : "ring", "path" : "images/galllery/4.png"}
+,{"name" : "ring", "path" : "images/galllery/5.png"}
+,{"name" : "ring", "path" : "images/galllery/6.png"}
+,{"name" : "ring", "path" : "images/galllery/7.png"}
+,{"name" : "ring", "path" : "images/galllery/8.png"}
+,{"name" : "ring", "path" : "images/galllery/9.png"}
+,{"name" : "ring", "path" : "images/galllery/10.png"}
+]`;
