b4b42980d5b67837ef3c5cb62f2a95b7ce8aa10d
Sonu Verma <sonu.verma0715@gmail.com>
2022-06-26 13:08:11 +0530

    slider related changes

--- pages/home.php
+++ pages/home.php
@@ -143,13 +143,14 @@
                 </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
+
+        <a class="carousel-control-next text-faded btn_next" href="#carouselExample" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+            <span class="sr-only"><</span>
+        </a>
+        <a class="carousel-control-prev btn_prev" href="#carouselExample" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">></span>
         </a>
     </div>
 </div>
